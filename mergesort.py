9494f0b7acc98857694a5dfae4b2fed87abbfced
yashashwini16 <yashaswinib1603@gmail.com>
2023-02-24 20:58:48 +0530

    sec pgm

---
+++ mergesort.py
@@ -0,0 +1,35 @@
+def m_sort(a):
+ for i in range(len(a)):
+   if i>1:
+     mid=len(a)//2
+     l_half=a[:mid]
+     r_half=a[mid:]
+     m_sort(l_half)
+     m_sort(r_half)
+     i=j=k=0
+ while i<len(l_half) and j<len(r_half):
+   if l_half[i]<r_half[j]:
+ 
+     a[k]=l_half[i]
+     i+=1
+   else:
+ 
+    a[k]=r_half[j]
+    j+=1
+    k+=1
+ while i<len(l_half):
+ 
+  a[k]=l_half[i]
+  i+=1
+  k+=1
+ while j<len(r_half):
+ 
+  a[k]=r_half[j]
+  j+=1
+  k+=1
+ 
+print("Enter elements into list")
+a=[int(x) for x in 
+input().split()]
+m_sort(a)
+print(a)
