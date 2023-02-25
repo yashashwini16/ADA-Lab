5c4334f6c7d1445e7e8810868e3d7584a3a6bbf8
yashashwini16 <yashaswinib1603@gmail.com>
2023-02-25 15:54:45 +0530

    4th pgm

---
+++ knapsack.py
@@ -0,0 +1,36 @@
+def show(W,n,wt,V):
+  i=n
+  w=W
+  print("The items entered into the knapsack are:")
+  while i>0 and w>0:
+     if V[i][w]!=V[i-1][w]:
+      print(i,"item with weight",wt[i])
+      w=w-wt[i]
+      i=i-1
+     else:
+        i=i-1
+  print("Weight taken into the sack is",W-w,"with having maximum value",V[n][W])
+def knapsack(W,n,val,wt):
+  V=[[0 for j in range(W+2)] for i in range(n+2)]
+  for i in range(n+1):
+    for w in range(W+1):
+      if wt[i]<=w:
+        V[i][w]=max(V[i-1][w],(val[i]+V[i-1][w-wt[i]]))
+      else:
+       V[i][w]=V[i-1][w]
+  print(V[i][w],end="\t")
+  print("\n")
+  show(W,n,wt,V)
+n=int(input("Enter the items:"))
+val=[0 for i in range(0,n+1,1)]
+wt=[0 for i in range(0,n+1,1)]
+print("Enter the values of products:")
+for i in range(1,n+1,1):
+ temp=int(input())
+val[i]=temp
+print("Enter the weights of products:")
+for i in range(1,n+1,1):
+ temp=int(input())
+ wt[i]=temp
+W=int(input("Enter the maximum capacity of knapsack:"))
+knapsack(W,n,val,wt)
